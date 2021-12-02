255328f8bff2fdca8d0f0fa46f558da3c6a921e7
Paul Piho <paul.piho@gmail.com>
2021-12-02 10:14:40 +0000

    IMPLEMENTED: derived statistic for division distribution

--- src/analytical.jl
+++ src/analytical.jl
@@ -26,11 +26,6 @@ mutable struct ConvergenceMonitor
 end
 
 mutable struct AnalyticalResults
-    # TODO: Structure holding the most necessary. Implement derived calculations
-    # based on thist struct.
-    #parameters::Vector{Float64}
-    #cme_model::Function
-    #division_time_dist::Function
     birth_dist::Vector{Float64}
     division_dist::Vector{Float64}
     growth_factor::Float64 
@@ -58,10 +53,19 @@ function division_dist(results::AnalyticalResults)
 
     cme = model.cme_model(results.birth_dist, model.tspan, model.parameters, solver.truncation)
     cme_solution = solve(cme, solver.solver; cb=PositiveDomain())# isoutofdomain=(u,p,t) -> any(x -> x .< 0, u), atol=1e-8) 
-#    division_time_dist(τ) = sum(first_passage_time(results.birth_dist, τ, parameters, cme_star_solution))
-#    div_time_dist(τ) = division_time_dist(results)
     return quadgk(
-        s -> 2 * exp(-results.growth_factor*s) * model.first_passage_time(results.birth_dist, s, model.parameters, cme_solution), 
+        s -> model.first_passage_time(results.birth_dist, s, model.parameters, cme_solution), 
+        model.tspan[1], model.tspan[2], rtol=solver.rtol)[1]
+end
+
+function division_dist_hist(results::AnalyticalResults) 
+    model = results.model
+    solver = results.solver
+
+    cme = model.cme_model(results.birth_dist, model.tspan, model.parameters, solver.truncation)
+    cme_solution = solve(cme, solver.solver; cb=PositiveDomain())# isoutofdomain=(u,p,t) -> any(x -> x .< 0, u), atol=1e-8) 
+    return quadgk(
+        s -> 2 * exp(-results.growth_factor * s) * model.first_passage_time(results.birth_dist, s, model.parameters, cme_solution), 
         model.tspan[1], model.tspan[2], rtol=solver.rtol)[1]
 end
 
@@ -83,7 +87,7 @@ function update_birth_dist!(model::AnalyticalModel, results::AnalyticalResults,
     boundary_cond_integrand(τ) = sum(
         model.partition_kernel.(collect.(axes(results.birth_dist))[1] .- 1, solver.truncation) .* 
         model.first_passage_time(results.birth_dist, τ, model.parameters, cme_solution) .* 2 .* exp(-results.growth_factor*τ))
-    
+
     results.birth_dist = quadgk(s -> boundary_cond_integrand(s), model.tspan[1], model.tspan[2], rtol=solver.rtol)[1]
     results.birth_dist = results.birth_dist / sum(results.birth_dist)
 end
