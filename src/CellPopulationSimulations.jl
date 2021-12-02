1e7dadfa95a8a7e845585cc80912e464b8952dd4
Paul Piho <paul.piho@gmail.com>
2021-12-02 10:17:18 +0000

    UPDATE: make added functions available

--- src/CellPopulationSimulations.jl
+++ src/CellPopulationSimulations.jl
@@ -16,11 +16,11 @@ include("cell_simulation.jl")
 include("analytical.jl")
 
 export AnalyticalModel, AnalyticalResults, AnalyticalSolverParameters
-export division_dist, division_time_dist
+export division_dist, division_dist_hist, division_time_dist, division_time_dist_hist
 export ThinningSampler, sample_first_arrival!
 export CellState, CellSimulationResults, CellSimulationParameters, CellSimulationModel
 export cellsize
-export simulate_population
+export simulate_population, simulate_population_slow
 export solvecme
 
 end # module
