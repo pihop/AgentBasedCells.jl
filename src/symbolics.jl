f44c20a587271d38ba7aca11085243fa984de801
Paul Piho <paul.piho@gmail.com>
2022-01-25 16:33:10 +0000

    FIX: export conflict

--- src/symbolics.jl
+++ src/symbolics.jl
@@ -1,5 +1,5 @@
 function gen_division_rate_function(symb_rate::Num, rn)
-    return @RuntimeGeneratedFunction build_function(symb_rate, states(rn), parameters(rn), ModelingToolkit.get_iv(rn);
+    return @RuntimeGeneratedFunction build_function(symb_rate, states(rn), Catalyst.parameters(rn), ModelingToolkit.get_iv(rn);
         conv = ModelingToolkit.states_to_sym(states(rn)),
         expression=Val{true})
 end
