f9c61600d8f8b051bd080f7b8609ff299b3f45f1
Paul Piho <paul.piho@gmail.com>
2022-01-26 14:09:18 +0000

    CHANGE: export added functionality

--- src/CellPopulationSimulations.jl
+++ src/CellPopulationSimulations.jl
@@ -34,7 +34,7 @@ include("experiment.jl")
 
 export AbstractExperimentSetup
 export AnalyticalModel, AnalyticalResults, AnalyticalSolverParameters
-export marginal_size_distribution!
+export marginal_size_distribution!, mean_marginal_size
 export growth_factor, division_dist, division_dist_hist, division_time_dist, division_time_dist_hist
 export ThinningSampler, sample_first_arrival!
 export CellState, CellSimulationResults, CellSimulationParameters, CellSimulationModel
@@ -43,10 +43,12 @@ export simulate_population, simulate_population_slow
 export solvecme
 export gen_division_rate_function
 
-export bifurcation_graph
+export EffectiveDilutionModel
+export root_finding!
 export Interval
 
 export PopulationExperimentSetup
+
 
 export BurstyReactionModel
 
