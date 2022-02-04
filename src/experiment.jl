eb86681c630abae56a03ab75f0f1f024e168dce1
Paul Piho <paul.piho@gmail.com>
2022-02-04 15:56:27 +0000

    ADD: parameter study struct

--- src/experiment.jl
+++ src/experiment.jl
@@ -7,7 +7,45 @@
     max_pop::Int64 
     truncation::Vector{Int64} 
     jitt::Float64 = 1e-4
-    molecular_model_rn 
+    molecular_model_rn::ReactionSystem
     division_rate
+    effective_dilution_rn::ReactionSystem
 end
 
+@kwdef struct ParameterStudySetup <: AbstractExperimentSetup 
+    bifurcation_index::Int64
+    parameter_span::Tuple{Float64, Float64}
+    simulation_pstep::Float64
+    effective_pstep::Float64 
+    parametrisations::Vector{Vector{Float64}} 
+    save_parameters::Vector{Float64} # Only use the first parameter in the parametrisations for saving. 
+    note::String
+
+    function ParameterStudySetup(;
+        bifurcation_index, 
+        parameter_span, 
+        simulation_pstep,
+        effective_pstep, 
+        parameter_vector, 
+        note) 
+        
+        parametrisations = [begin vec = deepcopy(parameter_vector); vec[bifurcation_index] = v; vec end for v in 
+            range(parameter_span[1], step = simulation_pstep, stop = parameter_span[2])]
+
+        new(bifurcation_index, 
+           parameter_span, 
+           simulation_pstep, 
+           effective_pstep, 
+           parametrisations, 
+           parametrisations[1],
+           note)
+    end
+end
+
+function simulation_prange(setup::ParameterStudySetup)
+    return range(setup.parameter_span[1], step = setup.simulation_pstep, stop = setup.parameter_span[2])
+end
+
+function effective_prange(setup::ParameterStudySetup)
+    return range(setup.parameter_span[1], step = setup.effective_pstep, stop = setup.parameter_span[2])
+end
