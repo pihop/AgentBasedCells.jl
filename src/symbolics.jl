be62e6e2c177b28361920f8aead639e8675d2a4c
Paul Piho <paul.piho@gmail.com>
2022-02-04 16:00:52 +0000

    ADD: broadcast for division rate generation

--- src/symbolics.jl
+++ src/symbolics.jl
@@ -1,5 +1,11 @@
-function gen_division_rate_function(symb_rate::Num, rn)
+function _gen_division_rate_function(symb_rate, rn::ReactionSystem)
     return @RuntimeGeneratedFunction build_function(symb_rate, states(rn), Catalyst.parameters(rn), ModelingToolkit.get_iv(rn);
         conv = ModelingToolkit.states_to_sym(states(rn)),
         expression=Val{true})
 end
+
+gen_division_rate_function(x, rn::ReactionSystem) = 
+    _gen_division_rate_function(x, rn)
+Broadcast.broadcasted(::typeof(gen_division_rate_function), x, rn) = 
+    broadcast(_gen_division_rate_function, x, Ref(rn))
+
