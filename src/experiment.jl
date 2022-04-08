7c491df4be688b01cf5031aae7cdfa7bd2da5641
Paul Piho <paul.piho@gmail.com>
2022-04-08 13:56:23 +0100

    Experiment setup changed. Still WIP

--- src/experiment.jl
+++ src/experiment.jl
@@ -1,101 +1,65 @@
-# Interface we need from the experiment setup.
-
-function parameters(::AbstractExperimentSetup) end
-function init_values(::AbstractExperimentSetup) end
-function simulation_tspan(::AbstractExperimentSetup) end
-function analytical_tspan(::AbstractExperimentSetup) end
-function Δt(::AbstractExperimentSetup) end
-function truncation(::AbstractExperimentSetup) end
-function jitt(::AbstractExperimentSetup) end
-function molecular_model(::AbstractExperimentSetup) end
-function symbolic_division_rate(::AbstractExperimentSetup) end
-
-function run_analytical(experiment::T; iters = 10, kwargs...) where T<:AbstractExperimentSetup
-    model = CellPopulationModel(
-        molecular_model(experiment), 
-        symbolic_division_rate(experiment), 
-        BinomialKernel(0.5))
-
-    approximation = FiniteStateApprox(truncation(experiment), analytical_tspan(experiment))  
-    solver = AnalyticalSolverParameters(truncation(experiment), iters; kwargs...)
-
-    return solvecme(model, experiment, approximation, solver)
-end
-
-function run_simulation(experiment::T) where T<:AbstractExperimentSetup
-    model = CellPopulationModel(
-        molecular_model(experiment), 
-        symbolic_division_rate(experiment), 
-        BinomialKernel(0.5))
-
-    initial_population = [CellState([0, ], 0.0, 0.0, [0.0, ], 0.0, ThinningSampler()), ]
-    simulation_model = CellSimulationModel(model, experiment, initial_population)
-    results = simulate_population(simulation_model)
-
-    sname = savename(experiment; allowedtypes = (Vector, String, Int, Float64))
-    save(datadir("sims", "cell_simulation", "$sname.jld2"), convert(Dict, results))
-    return results
-end
-
-@kwdef struct ParameterStudySetup <: AbstractExperimentSetup 
-    bifurcation_index::Int64
-    parameter_span::Tuple{Float64, Float64}
-    simulation_pstep::Float64
-    effective_pstep::Float64 
-    parametrisations::Vector{Vector{Float64}} 
-    save_parameters::Vector{Float64} # Only use the first parameter in the parametrisations for saving. 
-    note::String
-
-    function ParameterStudySetup(;
-        bifurcation_index, 
-        parameter_span, 
-        simulation_pstep,
-        effective_pstep, 
-        parameter_vector, 
-        note) 
-        
-        parametrisations = [begin vec = deepcopy(parameter_vector); vec[bifurcation_index] = v; vec end for v in 
-            range(parameter_span[1], step = simulation_pstep, stop = parameter_span[2])]
-
-        new(bifurcation_index, 
-           parameter_span, 
-           simulation_pstep, 
-           effective_pstep, 
-           parametrisations, 
-           parametrisations[1],
-           note)
-    end
-end
-
-function simulation_prange(setup::ParameterStudySetup)
-    return range(setup.parameter_span[1], step = setup.simulation_pstep, stop = setup.parameter_span[2])
-end
-
-function effective_prange(setup::ParameterStudySetup)
-    return range(setup.parameter_span[1], step = setup.effective_pstep, stop = setup.parameter_span[2])
-end
-
 @kwdef struct PopulationExperimentSetup <: AbstractExperimentSetup
     init::Vector{Float64} 
-    model_parameters::Vector{Float64} 
+    ps::Vector{Float64} 
     simulation_tspan::Tuple{Float64, Float64} 
     analytical_tspan::Tuple{Float64, Float64} 
+    iters::Int64
     Δt::Float64 
     max_pop::Int64 
     truncation::Vector{Int64} 
     jitt::Float64 = 1e-4
-    molecular_model::ReactionSystem
-    division_rate
-    effective_dilution::ReactionSystem
 end
 
-parameters(setup::PopulationExperimentSetup) = setup.model_parameters
-init_values(setup::PopulationExperimentSetup) = setup.init
-simulation_tspan(setup::PopulationExperimentSetup) = setup.simulation_tspan
-analytical_tspan(setup::PopulationExperimentSetup) = setup.analytical_tspan
-Δt(setup::PopulationExperimentSetup) = setup.Δt
-truncation(setup::PopulationExperimentSetup) = setup.truncation
-jitt(setup::PopulationExperimentSetup) = setup.jitt
-molecular_model(setup::PopulationExperimentSetup) = setup.molecular_model
-symbolic_division_rate(setup::PopulationExperimentSetup) = setup.division_rate
+function run_analytical(model, exp::T; kwargs...) where T <: AbstractExperimentSetup
+    approx = FiniteStateApprox(exp.truncation)
+    problem = AnalyticalProblem(model, exp.init, exp.ps, exp.analytical_tspan, approx) 
+    solver = AnalyticalSolver(exp.iters)
 
+    return solvecme(problem, solver)
+end
+
+function run_simulation(model, exp::T) where T <: AbstractExperimentSetup
+    init_pop = [CellState([0, ], 0.0, 0.0, [0.0, ], 0.0, CellPopulationSimulations.ThinningSampler()), ]
+    problem = CellSimulationProblem(model, init_pop, exp.ps, exp.simulation_tspan)  
+    solver = SimulationSolver(exp.Δt, exp.jitt, exp.max_pop) 
+
+    return simulate(probem, solver)
+end
+
+#@kwdef struct ParameterStudySetup <: AbstractExperimentSetup 
+#    bifurcation_index::Int64
+#    parameter_span::Tuple{Float64, Float64}
+#    simulation_pstep::Float64
+#    effective_pstep::Float64 
+#    parametrisations::Vector{Vector{Float64}} 
+#    save_parameters::Vector{Float64} # Only use the first parameter in the parametrisations for saving. 
+#    note::String
+#
+#    function ParameterStudySetup(;
+#        bifurcation_index, 
+#        parameter_span, 
+#        simulation_pstep,
+#        effective_pstep, 
+#        parameter_vector, 
+#        note) 
+#        
+#        parametrisations = [begin vec = deepcopy(parameter_vector); vec[bifurcation_index] = v; vec end for v in 
+#            range(parameter_span[1], step = simulation_pstep, stop = parameter_span[2])]
+#
+#        new(bifurcation_index, 
+#           parameter_span, 
+#           simulation_pstep, 
+#           effective_pstep, 
+#           parametrisations, 
+#           parametrisations[1],
+#           note)
+#    end
+#end
+#
+#function simulation_prange(setup::ParameterStudySetup)
+#    return range(setup.parameter_span[1], step = setup.simulation_pstep, stop = setup.parameter_span[2])
+#end
+#
+#function effective_prange(setup::ParameterStudySetup)
+#    return range(setup.parameter_span[1], step = setup.effective_pstep, stop = setup.parameter_span[2])
+#end
