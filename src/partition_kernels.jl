0b6926c795e28d04d6797e4d3b35cdc3dd10664d
Paul Piho <paul.piho@gmail.com>
2022-11-07 15:05:36 +0000

    partition kernel better implementation

--- src/partition_kernels.jl
+++ src/partition_kernels.jl
@@ -4,10 +4,16 @@ struct BinomialKernel <: AbstractPartitionKernel
     parameter::Float64
 end
 
-function _partition(kernel::BinomialKernel, x::Int64, truncation::Vector{Int64})
-    out = zeros(prod(truncation))    
+function _partition(kernel::BinomialKernel, x::Int64, truncation::Int64)
+    out = zeros(truncation)    
     out[1:x+1] = pdf(Binomial(x, kernel.parameter))
     return out
+end
+
+function _partition(kernel::BinomialKernel, x, truncation)
+    x_ = x .- ones(Int, length(truncation))
+    out_ = prod.(collect(Iterators.product(pdf.(Binomial.(x_, kernel.parameter))...)))
+    return PaddedView(0.0, out_, tuple(truncation...))
 end
 
 partition(kernel::AbstractPartitionKernel, x::Int64, truncation::Int64) = _partition(kernel, x, truncation)
