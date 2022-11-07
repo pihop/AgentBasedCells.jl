0279b4412731e4a40d4aa7bca4c806c6740a8c5d
Paul Piho <paul.piho@gmail.com>
2022-11-07 15:03:17 +0000

    interface

--- src/CellPopulationSimulations.jl
+++ src/CellPopulationSimulations.jl
@@ -8,17 +8,26 @@ using RuntimeGeneratedFunctions
 using DiffEqBase
 using JumpProcesses
 using OrdinaryDiffEq
+using Sundials
 using StochasticDiffEq
 using DiffEqCallbacks
 using ModelingToolkit
 using Catalyst
 using Distances
+using PaddedViews
 
+using Integrals
 using Cubature
+using Cuba
+using IntegralsCuba
+using IntegralsCubature
 
 using IntervalArithmetic: Interval
 using IntervalRootFinding
 using Roots
+
+using StaticArrays
+using PreallocationTools
 
 abstract type NonHomogeneousSampling end
 abstract type AbstractSimulationProblem end
@@ -31,7 +40,7 @@ include("partition_kernels.jl")
 export BinomialKernel 
 
 include("model.jl")
-export CellPopulationModel 
+export CellPopulationModel, MotherCellModel, DivisionRateMonotonicInc, DivisionRateBounded
 
 using Distributions
 include("thinning.jl")
@@ -46,6 +55,7 @@ export CellSimulationProblem, SimulationSolver
 export simulate
 include("analytical.jl")
 export AnalyticalProblem, AnalyticalSolver, solvecme
+export ToxicBoundaryUpper, ToxicBoundaryLower, Reinsert
 
 # Utils for the symbolically defined aspects of the model. 
 using Symbolics: value
@@ -63,7 +73,8 @@ export AbstractExperimentSetup, run_analytical, run_analytical_single, run_analy
 
 export AnalyticalModel, AnalyticalResults, AnalyticalSolverParameters
 export marginal_size_distribution!, mean_marginal_size
-export growth_factor, division_dist, division_dist_hist, division_time_dist, division_time_dist_hist
+export growth_factor, division_dist!, interdivision_time_dist, division_time_cdist!, joint_fpt_cdist!, division_dist_hist!, division_time_dist, division_time_dist_hist
+export birth_dist_hist!
 #export ThinningSampler, sample_first_arrival!
 export CellState, CellSimulationResults, CellSimulationParameters, CellSimulationProblem
 export cellsize, final_cell_sizes
