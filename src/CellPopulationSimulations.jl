a481ee99c1a0bf79898126da2e79ae23b29a056e
Paul Piho <paul.piho@gmail.com>
2024-02-13 10:39:34 +0000

    some imports not needed

--- src/CellPopulationSimulations.jl
+++ src/CellPopulationSimulations.jl
@@ -8,30 +8,25 @@ using RuntimeGeneratedFunctions
 using DiffEqBase
 using SciMLBase
 using JumpProcesses
-using OrdinaryDiffEq
-using Sundials
+import OrdinaryDiffEq: Rodas5
+
 using StochasticDiffEq
 using DiffEqCallbacks
-using ModelingToolkit
-using Catalyst
-using Distances
+import Catalyst: ReactionSystem
+import Distances: totalvariation
 using PaddedViews
 using PreallocationTools
 
 using Integrals
 using Cubature
-using Cuba
-using IntegralsCuba
-using IntegralsCubature
+using FastGaussQuadrature
 
-using IntervalArithmetic: Interval
-using IntervalRootFinding
 using Roots
 
-using StaticArrays
+import ArraysOfArrays: ArrayOfSimilarArrays
 using PreallocationTools
+using LoopVectorization
 
-using FunctionWrappers
 import FunctionWrappers: FunctionWrapper
 
 abstract type NonHomogeneousSampling end
@@ -43,7 +38,8 @@ abstract type AbstractAnalyticalApprox end
 
 include("cellstate.jl")
 export CellState
-export state_at_division, state_at_times
+export cell_index
+export state_at_division, state_at_times, lineage, state_time, division_time_state
 
 include("model.jl")
 export CellPopulationModel, MotherCellModel, DivisionRateMonotonicInc, DivisionRateBounded
@@ -63,19 +59,14 @@ export ToxicBoundaryRe, ToxicBoundaryDeath, Reinsert, Divide
 
 using FiniteStateProjection
 using SparseArrays
+include("make_sparse_mat.jl")
 include("analytical_approximations.jl")
 export FiniteStateApprox 
 
 # Utils for the symbolically defined aspects of the model. 
-using Symbolics: value
+import Symbolics: value
 using SymbolicUtils
 include("symbolics.jl")
-
-include("effective_dilution.jl")
-export EffectiveDilutionModel
-export root_finding
-include("stochastic_dilution.jl")
-export StochasticDilutionModel, birth_death_steady_state!, mean_steady_state
 
 include("experiment.jl")
 export AbstractExperimentSetup, run_analytical, run_analytical_single, run_analyticalerr_single, run_simulation, make_analytical_problem
@@ -84,13 +75,10 @@ export AnalyticalModel, AnalyticalResults, AnalyticalSolverParameters
 export marginal_size_distribution!, mean_marginal_size, compute_joint!, marginal_age_distribution, marginal_gamma, division_hazard
 export growth_factor, division_dist!, interdivision_time_dist, division_time_cdist!, joint_fpt_ancest_cdist!, division_dist_ancest!, division_time_dist, division_time_ancest, compute_growth_factor, fpt_dist_ancest
 export birth_dist_hist!
-#export ThinningSampler, sample_first_arrival!
 export CellSimulationResults, CellSimulationParameters, CellSimulationProblem
 export cellsize, final_cell_sizes
 export simulate_population, simulate_population_slow
 export cell_division_age
-#export solvecme
-#export gen_division_rate_function
 
 export Interval
 
@@ -98,7 +86,5 @@ export PopulationExperimentSetup
 
 export ParameterStudySetup
 export simulation_prange, effective_prange
-
-#export BurstyReactionModel
 
 end # module
